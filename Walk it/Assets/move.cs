a89db2473861c3bf2faf41a0375fad498b285d4f
30gold1991 <seaness1991@gmail.com>
2022-04-02 01:09:50 -0700

    walking tall
    again

---
+++ Walk it/Assets/move.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+    }
+}
