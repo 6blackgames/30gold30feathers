42e4eabafefcc367449cf5daf6978ffcd5e7d566
30gold1991 <seaness1991@gmail.com>
2022-04-03 02:59:43 -0700

    code

---
+++ parameters.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trans : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", move);
+        anim.SetFloat("horizontal", turn);
+    }
+}
